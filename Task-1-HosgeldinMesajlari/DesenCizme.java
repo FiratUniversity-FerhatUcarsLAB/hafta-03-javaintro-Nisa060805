fcd59349ff7c26e106e84e6a60af8c392758ec76
Nisanur Kırtepe <250541079@firat.edu.tr>
2025-10-22 23:29:16 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/DesenCizme.java
@@ -0,0 +1,11 @@
+public class DesenCizme {
+    public static void main(String[] args) {
+        System.out.println("   J");
+        System.out.println("  JA");
+        System.out.println(" JAV");
+        System.out.println("JAVA");
+        System.out.println(" JAV");
+        System.out.println("  JA");
+        System.out.println("   J");
+    }
+}
