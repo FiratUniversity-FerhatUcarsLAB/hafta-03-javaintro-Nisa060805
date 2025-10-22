19167c7bc0a156747e6f491c6bd5907f272b31f2
Nisanur Kırtepe <250541079@firat.edu.tr>
2025-10-22 23:47:37 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,4 @@
+Görev 3: Alan ve Çevre Hesaplamaları
+3.1 - DaireHesap.java: Yarıçap 5.5 olan dairenin alan ve çevresini hesaplar.
+3.2 - DikdortgenHesap.java: En=4.5, Boy=7.9 olan dikdörtgenin alan ve çevresini hesaplar.
+3.3 - UcgenHeron.java: Kenarları 3,4,5 olan üçgenin alanını Heron formülü ile hesaplar.
